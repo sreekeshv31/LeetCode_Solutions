e5bca47f07ddcdd3c6353bf29e2c093b8460dd6a
Sreekesh V <61742339+v-sreekesh@users.noreply.github.com>
2021-11-27 16:16:49 +0530

    Time: 23 ms (40.56%), Space: 14.6 MB (36.88%) - LeetHub

--- merge-intervals/merge-intervals.cpp
+++ merge-intervals/merge-intervals.cpp
@@ -4,24 +4,29 @@ public:
         vector<vector<int>> mergedIntervals;
         if(intervals.size()==0)
             return mergedIntervals;
-        
-        sort(intervals.begin(),intervals.end());
+        sort(intervals.begin(),intervals.end()); //O(nlogn)
         
         vector<int> tempInterval = intervals[0];
-        
         for(auto it : intervals)
         {
             if(it[0]<=tempInterval[1])
-                tempInterval[1] = max(it[1],tempInterval[1]);
+            {
+                tempInterval[1] = max(tempInterval[1],it[1]);
+            }
+                
             else
             {
                 mergedIntervals.push_back(tempInterval);
                 tempInterval = it;
-            
             }
-        
         }
         mergedIntervals.push_back(tempInterval);
         return mergedIntervals;
-    }
+            
+        }
+    
+   // T - O(nLogn) for sorting + O(N) for iterating
+       // S - O(N)
+        
+    
 };
