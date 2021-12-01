8a1a42a1178744e00de6edd9a002d48e38a81506
Sreekesh V <61742339+v-sreekesh@users.noreply.github.com>
2021-12-01 09:50:07 +0530

    Time: 8 ms (63.50%), Space: 8.3 MB (78.55%) - LeetHub

--- reverse-linked-list/reverse-linked-list.cpp
+++ reverse-linked-list/reverse-linked-list.cpp
@@ -13,14 +13,15 @@ public:
     ListNode* reverseList(ListNode* head) {
         ListNode *curr = head;
         ListNode *prev = NULL;
+        
         while(curr)
         {
             ListNode *next = curr->next;
             curr->next = prev;
             prev = curr;
-            curr = next;
+            curr=next;
         }
-        head = prev;
-        return head;
+        return prev;
+        
     }
 };
