5484e40eabdd36cf0b1ad9baca502d167081f4d6
Sreekesh V <61742339+v-sreekesh@users.noreply.github.com>
2021-12-29 08:26:06 +0530

    Time: 92 ms (54.57%), Space: 23.7 MB (28.59%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -3,11 +3,12 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
         sort(nums.begin(),nums.end());
-        for(int i = 0;i<(int)(nums.size())-2;i++)
+        
+        for(int i =0;i<(int)(nums.size())-2;i++)
         {
-            if(i==0||(i>0 && nums[i]!=nums[i-1]))
+            if(i==0 ||(i>0 && nums[i] != nums[i-1]))
             {
-                int low = i+1,high =  (int)(nums.size())-1,sum = 0 - nums[i];
+                int low = i+1,high = (int)(nums.size())-1,sum=0-nums[i];
                 while(low<high)
                 {
                     if(nums[low] + nums[high] == sum)
@@ -17,12 +18,14 @@ public:
                         temp.push_back(nums[low]);
                         temp.push_back(nums[high]);
                         res.push_back(temp);
+                        
                         while(low<high && nums[low] == nums[low+1])
                             low++;
                         while(low<high && nums[high]==nums[high-1])
                             high--;
                         
-                        low++;high--;
+                        low++;
+                        high--;
                     }
                     else if(nums[low] + nums[high] < sum)
                         low++;
