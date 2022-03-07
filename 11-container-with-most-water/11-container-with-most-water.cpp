c13a10c8d9ec863768c41e58c14cfe89a03bb678
Sreekesh V <61742339+v-sreekesh@users.noreply.github.com>
2022-03-07 15:53:50 +0530

    Time: 76 ms (96.60%), Space: 59.1 MB (39.34%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int a_pointer = 0;
-        int b_pointer = height.size()-1;
-        int max_area =0;
-        while(a_pointer<b_pointer)
-        {
-            if(height[a_pointer]<height[b_pointer])
-            {
-                max_area = max(max_area,height[a_pointer]*(b_pointer-a_pointer));
-                a_pointer++;
-            }
-            else
-            {
-                 max_area = max(max_area,height[b_pointer]*(b_pointer-a_pointer));
-                b_pointer--;
-                
-            }
+        int l = 0, r = height.size() - 1;
+        int ans = 0;
+        while(l < r) {
+            ans = max(ans, (r - l) * min(height[l], height[r]));
+            if(height[l] < height[r]) l++;
+            else r--;
         }
-        return max_area;
-        
+        return ans;
     }
 };
