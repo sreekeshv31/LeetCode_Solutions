4e017dd0186350e92b643eaa88f5e199a6bb2b9c
Sreekesh V <61742339+v-sreekesh@users.noreply.github.com>
2022-03-21 08:07:37 +0530

    Time: 34 ms (37.17%), Space: 10.8 MB (29.10%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // Record the last occurrence of each char.
-        unordered_map<char, size_t> last_occurrence;
-        size_t starting_idx = 0, ans = 0;
-        for (size_t i = 0; i < s.size(); ++i) {
-            auto it(last_occurrence.find(s[i]));
-            if (it == last_occurrence.cend()) {
-                last_occurrence.emplace_hint(it, s[i], i);
-            } else {  // s[i] appeared before. Check its validity.
-                if (it->second >= starting_idx) {
-                    ans = max(ans, i - starting_idx);
-                    starting_idx = it->second + 1;
-                }
-                it->second = i;
+        unordered_set<char> st;
+        int l =0,ans=0,n=s.size();
+        for(int i =0;i<n;i++)
+        {
+            while(st.count(s[i]))
+            {
+                ans = max(ans,i-l);
+                st.erase(s[l]);
+                l++;
             }
+            st.emplace(s[i]);
         }
-        ans = max(ans, s.size() - starting_idx);
-        return ans;
+        return max(ans,(int)st.size());
+        
     }
 };
