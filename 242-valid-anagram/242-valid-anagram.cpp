91bfb6c115af267febfd1d414d5e570f2d02dade
Sreekesh V <61742339+v-sreekesh@users.noreply.github.com>
2022-04-07 09:52:04 +0530

    Time: 16 ms (40.18%), Space: 7.4 MB (7.93%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        unordered_map<char, int> count;
+
+        for (const auto& c: s) {
+            ++count[tolower(c)];
+        }
+
+        for (const auto& c: t) {
+            --count[tolower(c)];
+            if (count[tolower(c)] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
