4496d014b8cb2da9974ef8f421eb5209e71f3a53
Sreekesh V <61742339+v-sreekesh@users.noreply.github.com>
2022-04-08 10:22:20 +0530

    Time: 60 ms (39.86%), Space: 19.5 MB (84.79%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -2,27 +2,26 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>> groups;
-        
-        for(const auto& str:strs)
+        for(auto const& str:strs)
         {
             string tmp{str};
             sort(tmp.begin(),tmp.end());
             groups[tmp].emplace_back(str);
         }
         
-        vector<vector<string>> anagrams;
         
-        for(const auto& kvp:groups)
+        vector<vector<string>> anagrams;
+        for(auto const& kvp:groups)
         {
             vector<string> group;
-            for(const auto& str : kvp.second)
+            for(auto const& str:kvp.second)
             {
                 group.emplace_back(str);
+                
             }
             sort(group.begin(),group.end());
             anagrams.emplace_back(move(group));
         }
         return anagrams;
-        
     }
 };
