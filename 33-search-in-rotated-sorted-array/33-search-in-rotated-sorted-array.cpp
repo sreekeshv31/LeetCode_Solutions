db6e24e35290bdec96786e3779b60cc756e30bd3
Sreekesh V <61742339+v-sreekesh@users.noreply.github.com>
2022-04-12 09:43:44 +0530

    Time: 8 ms (33.34%), Space: 11.1 MB (29.49%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,23 +1,29 @@
-// Time:  O(logn)
-// Space: O(1)
-
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
+        int left = 0,right = nums.size()-1;
+        while(left<=right)
+        {
+            int mid = left + (right-left)/2;
+            if(nums[mid]==target)
                 return mid;
-            } else if ((nums[mid] >= nums[left] && nums[left] <= target && target < nums[mid]) ||
-                       (nums[mid] < nums[left] && !(nums[mid] < target && target <= nums[right]))) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+            if(nums[left]<=nums[mid])
+            {
+                if(target<=nums[mid] && target>=nums[left])
+                    right = mid -1;
+                else
+                    left = mid +1;
+                    
+            }
+            else
+            {
+                if(target>=nums[mid] && target<=nums[right])
+                    left = mid +1;
+                else
+                    right = mid -1;
             }
         }
-
         return -1;
+        
     }
 };
