4c219158cd95bb43e4a9cd96170ae5065027760e
Sreekesh V <61742339+v-sreekesh@users.noreply.github.com>
2022-04-16 16:16:52 +0530

    Time: 7 ms (80.36%), Space: 10.7 MB (96.59%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,32 +1,27 @@
-// Time Complexity: O(n!)
-// Space Complexity: O(1)
-
 class Solution {
-public:
-    void findCombination(int ind,int target,vector<int>&arr,vector<vector<int>>&ans,vector<int>&ds)
-    {
-        if(ind==arr.size())
-        {
-            if(target==0)
-            {
-                ans.push_back(ds);
+public: 
+    void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int>&ds) {
+        if(ind == arr.size()) {
+            if(target == 0) {
+                ans.push_back(ds); 
             }
-            return;
+            return; 
         }
-        //pick elements
-        if(arr[ind]<=target)
-        {
-            ds.push_back(arr[ind]);
-            findCombination(ind,target-arr[ind],arr,ans,ds);
-            ds.pop_back();
+        // pick up the element 
+        if(arr[ind] <= target) {
+            ds.push_back(arr[ind]); 
+            findCombination(ind, target - arr[ind], arr, ans, ds); 
+            ds.pop_back(); 
         }
-        findCombination(ind+1,target,arr,ans,ds);
-    }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> ds;
-        findCombination(0,target,candidates,ans,ds);
-        return ans;
         
+        findCombination(ind+1, target, arr, ans, ds); 
+        
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans; 
+        vector<int> ds; 
+        findCombination(0, target, candidates, ans, ds); 
+        return ans; 
     }
 };
