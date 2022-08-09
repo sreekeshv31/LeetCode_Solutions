51c103eb9542ac88aaaae6ab428bd2b58b2c40b2
Sreekesh V <61742339+v-sreekesh@users.noreply.github.com>
2022-08-09 07:33:44 +0530

    Time: 507 ms (69.44%), Space: 0B (100.00%) - LeetHub

--- 180-consecutive-numbers/180-consecutive-numbers.sql
+++ 180-consecutive-numbers/180-consecutive-numbers.sql
@@ -1,3 +1,18 @@
-SELECT DISTINCT l1.Num  as ConsecutiveNums
+/*SELECT DISTINCT l1.Num  as ConsecutiveNums
 FROM Logs l1, Logs l2, Logs l3  
-WHERE l1.Id + 1 = l2.Id AND l2.Id + 1 = l3.Id AND l1.Num = l2.Num AND l2.Num = l3.Num 
+WHERE l1.Id + 1 = l2.Id AND l2.Id + 1 = l3.Id AND l1.Num = l2.Num AND l2.Num = l3.Num */
+
+select
+    distinct num as ConsecutiveNums 
+from 
+(
+    select 
+        num, 
+        id - cast(row_number() over (order by num, id) as SIGNED) as rk 
+    from 
+        logs
+) diff_grp
+group by 
+    num,rk
+having 
+    count(*) >= 3
