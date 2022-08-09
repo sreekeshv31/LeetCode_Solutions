a8f301cd38f72e2582b94ddf0183038b86a13071
Sreekesh V <61742339+v-sreekesh@users.noreply.github.com>
2022-08-09 07:48:09 +0530

    Time: 1009 ms (6.29%), Space: 0B (100.00%) - LeetHub

--- 178-rank-scores/178-rank-scores.sql
+++ 178-rank-scores/178-rank-scores.sql
@@ -1,4 +1,8 @@
-SELECT S1.Score, (
+/*SELECT S1.Score, (
     SELECT COUNT(DISTINCT Score) FROM Scores WHERE Score >= S1.Score) AS `Rank`
 FROM Scores S1
-ORDER BY S1.Score DESC
+ORDER BY S1.Score DESC*/
+
+SELECT Score,  (SELECT COUNT(DISTINCT(Score)) FROM  Scores b WHERE b.Score > a.Score) + 1 AS `Rank`
+       FROM Scores a
+       ORDER by Score DESC
