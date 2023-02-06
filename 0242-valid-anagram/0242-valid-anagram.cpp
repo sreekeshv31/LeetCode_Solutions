5c463a221510060a402819334c0167d44d51bcda
Sreekesh V <61742339+v-sreekesh@users.noreply.github.com>
2023-02-06 06:41:59 +0530

    Time: 7 ms (83.19%), Space: 7.3 MB (61.72%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())
+            return false;
+        
+        unordered_map<char,int> count;
+        
+        for(const auto& c:s)
+        {
+            ++count[c];
+        }
+        for(const auto& c:t)
+        {
+            --count[c];
+            if(count[c]<0)
+            {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
