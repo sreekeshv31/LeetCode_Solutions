d002669d3551b2c915a38c7bb25cd9685219c778
Sreekesh V <61742339+v-sreekesh@users.noreply.github.com>
2023-04-01 09:14:47 +0530

    Time: 137 ms (67.53%), Space: 57.3 MB (71.91%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> num_set(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());
         
-        return num_set.size()!=nums.size(); // T- O(N) and S- O(N)
+        return unique(nums.begin(),nums.end())!=nums.end(); // T-O(nlogn) S - O(1)
         
     }
 };
