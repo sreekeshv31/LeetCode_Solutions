0d361179af998b0842460634cbc22702418f78b9
Sreekesh V <61742339+v-sreekesh@users.noreply.github.com>
2024-06-18 08:44:26 +0530

    Time: 82 ms (37.31%), Space: 27.2 MB (79.50%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,40 +3,41 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> result;
         sort(nums.begin(),nums.end());
-        for(int i = nums.size()-1;i>=2;--i)
+        for(int i =nums.size()-1;i>=2;--i)
         {
-            if(i+1 < nums.size() && nums[i] == nums[i+1] )
+            if(i+1<nums.size() && nums[i] == nums[i+1])
             {
                 continue;
             }
-        
-        const auto& target = -nums[i];
-        int left =0, right = i-1;
-        while(left<right)
-        {
-            if(nums[left]+nums[right] < target)
+            int left = 0, right = i-1;
+            const auto& target = -nums[i];
+            while(left<right)
             {
-                ++left;
-            }
-            else if(nums[left]+nums[right] > target)
-            {
-                --right;
-            }
-            else
-            {
-                result.push_back({nums[left],nums[right],nums[i]});
-                ++left,--right;
-                while(left<right && nums[left]==nums[left-1])
+                if(nums[left] + nums[right] < target )
                 {
                     ++left;
                 }
-                while(left<right && nums[right] == nums[right+1])
+                else if(nums[left] + nums[right] > target )
                 {
                     --right;
                 }
+                else
+                {
+                    result.push_back({nums[left],nums[right],nums[i]});
+                    ++left,--right;
+                    while(left<right && nums[left] == nums[left-1])
+                    {
+                        ++left;
+                    }
+                    while(left<right && nums[right] == nums[right+1])
+                    {
+                        --right;
+                    }
+                
             }
         }
-        }
+        
+    }
         return result;
     }
 };
